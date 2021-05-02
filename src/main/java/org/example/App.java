88758fa74bb8da78e09b7e2ba994e0d15b7aeadc
Mikail Mitrahovich <bladesvet@gmail.com>
2021-05-02 21:19:41 +0300

    feat: add server impl

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.io.IOException;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+public class App {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		Server server = ServerBuilder.forPort(8080).addService(new GreetingServiceImpl()).build();
+
+		server.start();
+
+		System.out.println("Server stated");
+
+		server.awaitTermination();
+	}
+
+}
